e47b89b4d2e26c51daca8f0e19b1e2f0a741afb0
RickDoesStuff <rickyjaquino@gmail.com>
2024-03-20 19:07:17 -0400

    mysh updated with a test script file

--- mysh.c
+++ mysh.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
+//#include <dirent.h>
+#include <unistd.h>
 
 int interactiveMode(char *path);
 
@@ -15,20 +18,27 @@ int main(int argc, char **argv) {
         
         printf("arg found:%s\n",argv[1]);
         path = (char *)(argv[1]);
+        printf("1\n");
         int fd = open(path,O_RDONLY);
+
+
         if (fd < 0) {printf("error opening file: %s\n",path);}
+
 
         // check if that file is a tty or a batch file
         if (isatty(fd) == 1) {
+            
             // it is a tty
             printf("running in interactive mode, given terminal path\n");
-            // here I may need to change the output from the current terminal to the terminal I was given I think
-            // using 
-            // dup2(fd, fd);
+            
+            // here I may need to change the output from the current terminal to the terminal I was given
+            if (dup2(fd, STDOUT_FILENO) < 0) {perror("Failed to dup");}
 
         } else {
             // is not a tty
             printf("running in batch mode\n");
+            
+            printf("5\n");
             interactive = 0;
         }
     } else {
@@ -44,30 +54,45 @@ int main(int argc, char **argv) {
 }
 
 /**
- * print x amt of blank lines
-*/
-void blankLines(int count){
-    for (int i=0;i<count;i++)
-        printf("\n");
-}
-/**
  * Being run in interactive mode
  * return 0 on fail
  * return 1 on success
 */
 int interactiveMode(char *path) {
-    blankLines(4);
-    printf("Welcome to mysh");
-    blankLines(9);
+
+    printf("\n\nWelcome to mysh\n\n");
 
     int isRunning = 1;
-    char *userMessage;
-    while (isRunning)
-    {   
-        // exit from loop
+    char *userMessage = malloc(sizeof(char*) * 256);
+    while (isRunning) {
         printf("mysh> ");
-        if(strcmp(userMessage, "exit") == 0) {isRunning = 0;}
+
+        if (!fgets(userMessage, 256, stdin)) {
+            printf("Error reading input or EOF\n");
+            isRunning = 0;
+            break; // Exit the loop on EOF or error
+        }
+
+        // Remove newline character if present
+        userMessage[strcspn(userMessage, "\n")] = '\0';
+        printf("read:%s\n",userMessage);
+        if(strcmp(userMessage, "exit") == 0) {
+            isRunning = 0;
+            break;
+        }
+
+
+
+        // use the cmd in the terminal
+
+        //int execl(const char *path, const char *arg, ...);
+        if (execl("/usr/bin/env", "env", "pwd", NULL) == -1) {
+            if (system(userMessage) == -1) {
+                perror("system call failed");
+            }
+        }
     }
+
 
 
 
